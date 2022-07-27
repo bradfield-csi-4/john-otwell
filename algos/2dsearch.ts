c5066a2bd4d25739784ab41f6cfdd8a198e0f982
John Otwell <jotwell@chippercash.com>
2022-07-27 16:01:52 -0600

    2d search algorithm2d search algorithm

---
+++ algos/2dsearch.ts
@@ -0,0 +1,30 @@
+function searchMatrix(matrix: number[][], target: number): boolean {
+    const midMat = Math.floor(matrix.length / 2)
+    if (matrix.length === 1) {
+        return binarySearch(target, matrix[0])
+    } else if (matrix[midMat][0] > target) {
+        return searchMatrix(matrix.slice(0, midMat), target)
+    } else if (matrix[midMat][0] < target && matrix[midMat + 1][0] < target) {
+        return searchMatrix(matrix.slice(midMat + 1), target)
+    } else {
+        return binarySearch(target, matrix[midMat])
+    }
+};
+
+function binarySearch(n: number, ns: number[]): boolean {
+    const mid = Math.floor(ns.length / 2)
+    if (n === ns[mid]) {
+        return true
+    }
+    else if (ns.length === 0) {
+        return false
+    } else if (n > ns[mid]) {
+        return binarySearch(n, ns.slice(mid + 1))
+    } else {
+        return binarySearch(n, ns.slice(0, mid))
+    }
+}
+
+const matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
+console.log(binarySearch(7, [6, 7, 10]))
+console.log(searchMatrix(matrix, 12))
