64d19454853ccfdf95d4b9978443e4ccba58bc03
John Otwell <jotwell@chippercash.com>
2022-08-04 18:01:28 -0600

    Better solution to 2d search

--- algos/2dsearch.ts
+++ algos/2dsearch.ts
@@ -1,14 +1,17 @@
 function searchMatrix(matrix: number[][], target: number): boolean {
-    const midMat = Math.floor(matrix.length / 2)
-    if (matrix.length === 1) {
-        return binarySearch(target, matrix[0])
-    } else if (matrix[midMat][0] > target) {
-        return searchMatrix(matrix.slice(0, midMat), target)
-    } else if (matrix[midMat][0] < target && matrix[midMat + 1][0] < target) {
-        return searchMatrix(matrix.slice(midMat + 1), target)
-    } else {
-        return binarySearch(target, matrix[midMat])
-    }
+    // const midMat = Math.floor(matrix.length / 2)
+    // if (matrix.length === 1) {
+    //     return binarySearch(target, matrix[0])
+    // } else if (matrix[midMat][0] > target) {
+    //     return searchMatrix(matrix.slice(0, midMat), target)
+    // } else if (matrix[midMat][0] < target && matrix[midMat + 1][0] < target) {
+    //     return searchMatrix(matrix.slice(midMat + 1), target)
+    // } else {
+    //     return binarySearch(target, matrix[midMat])
+    // }
+
+    // a less stupid approach
+    return binarySearch(target, matrix.reduce((p, n) => p.concat(n)))
 };
 
 function binarySearch(n: number, ns: number[]): boolean {
@@ -26,5 +29,4 @@ function binarySearch(n: number, ns: number[]): boolean {
 }
 
 const matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
-console.log(binarySearch(7, [6, 7, 10]))
-console.log(searchMatrix(matrix, 12))
+console.log(searchMatrix(matrix, 1))
